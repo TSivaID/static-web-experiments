fc80d7e5c0d8c9e56c531b2da9a2a42126edbc48
TSivaID <TSivaID@users.noreply.github.com>
2023-03-23 13:25:58 +0000

    deploy: 7bace13cc395834abda46abfcea7931a08326677

---
+++ index.4d405677.js
@@ -0,0 +1,2 @@
+(new class{trackEvent(e){console.log(`Tracking event: ${e}`)}constructor(){}}).trackEvent("Page loaded");
+//# sourceMappingURL=index.4d405677.js.map
---
+++ index.df0ffcea.js
@@ -0,0 +1,2 @@
+(new class{trackEvent(e){console.log(`Tracking event: ${e}`)}constructor(){}}).trackEvent("Page loaded");
+//# sourceMappingURL=index.df0ffcea.js.map
