5dfcc4db418254a0a99456b6c8266e82618c38ab
Thathees Siva <tsiva@postmedia.com>
2023-03-27 00:52:12 -0400

    try with sendBeacon

--- src/analytics/providers/beeceptor-analytics.ts
+++ src/analytics/providers/beeceptor-analytics.ts
@@ -1,6 +1,10 @@
 import { IAnalyticsProvider } from './ianalytics-provider';
 import { logger } from '../../utils/logger';
 
+interface BeeceptorAnalyticsConfig {
+  useSendBeacon?: boolean;
+}
+
 /**
  * BeeceptorAnalytics is a simple Mock analytics provider that sends events to a
  * Beeceptor endpoint. This is useful for testing analytics in a development environment.
@@ -8,6 +12,11 @@ import { logger } from '../../utils/logger';
 export class BeeceptorAnalytics implements IAnalyticsProvider {
   public readonly name = 'BeeceptorAnalytics';
   private readonly endpoint: string = 'https://static-web-experiments.free.beeceptor.com/events/';
+  private readonly useSendBeacon: boolean;
+
+  constructor(config?: BeeceptorAnalyticsConfig) {
+    this.useSendBeacon = config?.useSendBeacon || true;
+  }
 
   initialize(): void {
     logger.info('BeeceptorAnalytics initialized');
@@ -15,23 +24,40 @@ export class BeeceptorAnalytics implements IAnalyticsProvider {
 
   async trackEvent(eventName: string, data?: Record<string, unknown>): Promise<void> {
     if (!data?.beeceptor_analytics) return Promise.resolve();
-    const providerData = data.beeceptor_analytics;
-    try {
-      logger.info(`BeeceptorAnalytics sending event: ${eventName} with data: ${JSON.stringify(providerData)}`);
-      const response = await fetch(`${this.endpoint}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ eventName, data: providerData }),
-      });
-      if (!response.ok) {
-        logger.error(`BeeceptorAnalytics failed to track event: ${response.statusText}`);
+    const providerData = { ...data.beeceptor_analytics, useSendBeacon: this.useSendBeacon };
+
+    if (this.useSendBeacon && navigator.sendBeacon) {
+      const blob = new Blob([JSON.stringify({ eventName, data: providerData })], { type: 'application/json' });
+
+      const success = navigator.sendBeacon(`${this.endpoint}`, blob);
+
+      if (success) {
+        logger.info(
+          `BeeceptorAnalytics sent event using sendBeacon: ${eventName} with data: ${JSON.stringify(providerData)}`
+        );
+      } else {
+        logger.error(`BeeceptorAnalytics failed to send event using sendBeacon: ${eventName}`);
+      }
+
+      return Promise.resolve();
+    } else {
+      try {
+        logger.info(`BeeceptorAnalytics sending event: ${eventName} with data: ${JSON.stringify(providerData)}`);
+        const response = await fetch(`${this.endpoint}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ eventName, data: providerData }),
+        });
+        if (!response.ok) {
+          logger.error(`BeeceptorAnalytics failed to track event: ${response.statusText}`);
+          return Promise.resolve();
+        }
+      } catch (error) {
+        logger.error(`BeeceptorAnalytics failed to track event: ${(error as Error).message}`);
         return Promise.resolve();
       }
-    } catch (error) {
-      logger.error(`BeeceptorAnalytics failed to track event: ${(error as Error).message}`);
-      return Promise.resolve();
     }
   }
 }
