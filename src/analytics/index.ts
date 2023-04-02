d6bf3af0e64c0a2381cc70b88a18b229cdfea42b
Thathees Siva <tsiva@postmedia.com>
2023-04-02 06:18:17 -0400

    formatting

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -9,10 +9,13 @@ import { logger } from '../utils/logger';
 
 export class WebAnalyticsFacade {
   async initialize(options?: Record<string, unknown>): Promise<AnalyticsService> {
+    // Initialize analytics providers
     const mParticleAnalytics = new MParticleAnalytics({ shouldForceUpload: false });
     const mockApiAnalyticsWithFetch = new MockApiAnalytics({ useSendBeacon: false });
     const mockApiAnalyticsWithSendBeacon = new MockApiAnalytics({ useSendBeacon: true });
     const dummyAnalytics = new DummyAnalytics();
+
+    // Initialize analytics service
     const analyticsService = new AnalyticsService([
       mParticleAnalytics,
       mockApiAnalyticsWithFetch,
@@ -20,11 +23,16 @@ export class WebAnalyticsFacade {
       dummyAnalytics,
     ]);
     await analyticsService.initialize(options);
+
+    // Initialize events variables parser
     const eventVariablesParser = new EventsVariablesParser();
+
+    // Initialize triggers and data layer manager
     const triggerBinder = new TriggerBinder(analyticsService, eventVariablesParser);
     triggerBinder.initialize();
     const dataLayerManager = new DataLayerManager(analyticsService, eventVariablesParser);
     dataLayerManager.initialize();
+
     logger.info('Analytics initialized');
     return Promise.resolve(analyticsService);
   }
